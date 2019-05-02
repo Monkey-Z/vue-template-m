79d1caded37d78be76bd64401ba5c7027d0d6082
Monkey-Z <Monkey-Z@users.noreply.github.com>
2019-05-02 21:56:21 +0900

    init vue project and add sass config

---
+++ src/components/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Welcome</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang='scss' scoped>
+</style>
